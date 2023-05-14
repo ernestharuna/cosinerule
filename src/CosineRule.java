12d2416d38e74fa573756097fb9166e3b3b7471d
Ernest Haruna <ernestharuna1@gmail.com>
2023-05-14 10:00:09 +0100

    Final commit Added clear button# Added events to Go and Clear button Approx the value of the answer

--- src/CosineRule.java
+++ src/CosineRule.java
@@ -13,24 +13,30 @@ public class CosineRule extends JFrame {
     private JTextField includedAngle;
     private JTextField otherSide;
 
+    private JButton clear;
     private JButton go;
 
     public CosineRule() {
         super("Cosine Rule");
+        // Input Labels
         firstSideLabel = new JLabel("Side AB:");
         secondSideJLabel = new JLabel("Side AC:");
         includedAngleLabel = new JLabel("Side BAC:");
         otherSideLabel = new JLabel("Side BC:");
 
+        // Input Fields
         firstSide = new JTextField(10);
         secondSide = new JTextField(10);
         includedAngle = new JTextField(10);
         otherSide = new JTextField(10);
 
+        // Primary button
         go = new JButton("Go");
+        clear = new JButton("Clr");
 
         setLayout(new FlowLayout());
 
+        // Input fields and labels
         add(firstSideLabel);
         add(firstSide);
 
@@ -45,13 +51,32 @@ public class CosineRule extends JFrame {
 
         add(go);
 
+        // Size
         setVisible(true);
         setLocation(300, 300);
 
         setSize(300, 300);
+
+        go.addActionListener(new Handler());
+        clear.addActionListener(new Handler());
     }
 
-    public static void main(String[] args) {
-        new CosineRule();
+    private class Handler implements ActionListener {
+        public void actionPerformed(ActionEvent ae) {
+            if (ae.getSource() == go) {
+                double ab = Double.parseDouble(firstSide.getText());
+                double ac = Double.parseDouble(secondSide.getText());
+                double angle = Double.parseDouble(includedAngle.getText());
+                double bc = ab * ab + ac * ac - 2 * ab * ac * Math.cos((angle * 180) / Math.PI);
+                otherSide.setText(String.format("%.2f", bc) + "");
+
+                // otherSide.setText(bc + "");/
+            }
+            if (ae.getSource() == clear) {
+                firstSide.setText("");
+                secondSide.setText("");
+                includedAngle.setText("");
+            }
+        }
     }
 }
